2f2618e87e08fdfae7b96e4639ec5430447c654b
WoLF1K88 <79374217+WoLF1K88@users.noreply.github.com>
2021-02-25 22:38:37 +0300

    Update ToDoWoLFiK.py

--- ToDoWoLFiK.py
+++ ToDoWoLFiK.py
@@ -1,34 +1,35 @@
 todos =[]#пустой список дел
-
+menu = "1. Print \n" \
+       "2. Add \n" \
+       "3. Change \n" \
+       "4. Delete \n" \
+       "0. Exit"
 def print_todos():
-    pass #печатать текст
+   print(todos)#печатать список
 def add_todo(text):
-    pass
+        text = input("Введите задачу:" )#добавление задачи
+        todos.append(text)
+        return text
+while True:
+    print(menu)
+    command = input("Enter command:  " )
+    if command not in ("1","2","3","4","0"):
+            print("Error")
+    elif command == "1": 
+            print_todos()
+    elif command == "2":
+            add_todo
+       
 
-#todos.append(text):
-    pass
+
 
 def change_todo(index, text):
     pass
 def delete_todo(index):
     pass
 
-menu = "1. Print \n" \
-       "2. Add \n" \
-       "3. Change \n" \
-       "4. Delete \n" \
-       "0. Exit"
+
 #вместо ИФОВ МОЖНО ЗАВЕЗТИ СЛОВАРЬ
 #dd= {"1": print_todos,
 # "2:.."}
 #dd[command]
-while True:
-    print (menu)
-    command = input ("Enter command")
-    if command not in ["1","2","3","4","0"]:
-        print("Error")
-        continue
-    if command == "1": 
-        print_todos()
-    elif command == "...":
-        continue
